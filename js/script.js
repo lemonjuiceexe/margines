beee7ca649408413369f1210b4b9455756ae5c3d
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2023-06-16 11:09:13 +0200

    Add footer responsiveness, don't animate on mobile

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ const yearElement = document.querySelector('.current-year');
 yearElement.innerText = new Date().getFullYear();
 
 // Animate on scroll
-function observerCallback(entries){
+function observerCallback(entries) {
     entries.forEach(entry => {
         console.log(entry.target);
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add("scrolled-into");
             return;
         }
@@ -20,8 +20,13 @@ const observerQuick = new IntersectionObserver(observerCallback, {
     threshold: 0
 });
 
-observerLazy.observe(document.querySelector(".main-header"));
-observerLazy.observe(document.querySelector(".about-content"));
-observerLazy.observe(document.querySelector(".for-writers-header"));
-document.querySelectorAll(".common-mistakes .rule").forEach(el => observerLazy.observe(el));
-document.querySelectorAll("section").forEach(el => observerQuick.observe(el));
+// Don't animate on mobile devices
+if (!window.matchMedia("only screen and (max-width: 960px)").matches) {
+    observerLazy.observe(document.querySelector(".main-header"));
+    observerLazy.observe(document.querySelector(".about-content"));
+    observerLazy.observe(document.querySelector(".for-writers-header"));
+    document.querySelectorAll(".common-mistakes .rule").forEach(el => observerLazy.observe(el));
+    document.querySelectorAll("section").forEach(el => observerQuick.observe(el));
+} else{
+    document.querySelectorAll(".about-content, .for-writers-header, .common-mistakes .rule, section").forEach(el => el.classList.add("scrolled-into") );
+}
